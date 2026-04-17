9b171f470a477f4103ce60b90bb15cd696a70922
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-04-17 23:43:15 +0000

    feat: add Grade, TeamMate, and Filere validation schemas using VineJS

--- backend/app/validators/grade.ts
+++ backend/app/validators/grade.ts
@@ -1,1 +1,5 @@
 import vine from '@vinejs/vine'
+
+export const GradeValidator = vine.create({
+    name: vine.string().minLength(3).maxLength(100)
+})  
--- backend/app/validators/team_mate.ts
+++ backend/app/validators/team_mate.ts
@@ -1,1 +1,9 @@
 import vine from '@vinejs/vine'
+
+export const TeamMateValidator = vine.create({
+    first_name: vine.string().minLength(3).maxLength(100),
+    last_name: vine.string().minLength(3).maxLength(100),
+    grade_id: vine.string(),
+    filere_id: vine.string(),
+    matricule: vine.string().minLength(3).maxLength(100).unique({ table: 'team_mates', column: 'matricule'})
+})  
