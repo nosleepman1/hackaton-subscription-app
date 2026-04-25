874b3c81621cde44613016f9c4fc74e527ea1ce2
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-04-25 12:56:19 +0000

    refactor: remove redundant utility functions from the codebase

--- server/app/validators/interrested.ts
+++
@@ -1,7 +0,0 @@
-import vine from '@vinejs/vine'
-
-
-export const InterrestedValidator = vine.create({
-    userId: vine.string().uuid().exists({ table: 'users', column: 'id' }),
-    projectId: vine.string().uuid().exists({ table: 'projects', column: 'id' })
-})      
--- server/app/validators/theme.ts
+++
@@ -1,7 +0,0 @@
-import vine from '@vinejs/vine'
-
-
-export const ThemeValidator = vine.create({
-    name: vine.string().minLength(3).maxLength(255),    
-    description: vine.string().minLength(3)
-})
