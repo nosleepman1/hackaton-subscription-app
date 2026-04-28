40371fe997b1b95c757af21ed92e2b5ad1c6e85a
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-04-28 13:12:15 +0000

    connect compoents and pages with react router with protected and guest routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
-import AppRoutes from "./routes/routes";
+// Point d'entrée de l'application
+// Enveloppe l'ensemble avec les providers de contexte (auth, thème) et le routeur
 
-function App() {
-  return <AppRoutes />;
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { ThemeProvider } from './context/ThemeContext'
+import { ToastProvider } from './components/ui/Toast'
+import AppRouter from './router/AppRouter'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      {/* Provider de thème : doit envelopper toute l'app pour appliquer les classes CSS */}
+      <ThemeProvider>
+        {/* Provider d'authentification : gère le token Sanctum et le rôle utilisateur */}
+        <AuthProvider>
+          <ToastProvider>
+            <AppRouter />
+          </ToastProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
