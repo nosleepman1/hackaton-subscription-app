ab774d5b3884a78130b2206627d158c528c6e4a3
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-05-06 13:13:49 +0000

    feat: implement registration page with user information form and validation logic

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -5,8 +5,38 @@ import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import useRegister from "@/hooks/auth/useRegister"
+import type { RegisterRequest, User } from "@/types/auth"
 
 const Register = () => {
+
+    const [matricule, setMatricule] = useState<string>("")
+    const [lastname, setLastname] = useState<string>("")
+    const [firstname, setFirstname] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [grade, setGrade] = useState<string>("")
+    const [filiere, setFiliere] = useState<string>("")
+    const [phone, setPhone] = useState<string>("")
+
+    const {loading, register, error, success} = useRegister()
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const user : RegisterRequest = {
+            matricule,
+            lastname,
+            firstname,
+            email,
+            password,
+            grade,
+            filiere,
+            phone
+        }
+        register(user)    
+    }
+
     return (
 
         <Card className="w-full py-10 px-2 max-w-md mx-auto">
@@ -15,38 +45,81 @@ const Register = () => {
             </CardHeader>
             <CardContent>
                 <p className="text-center mt-2 text-sm text-muted-foreground">Inscrivez-vous à votre compte </p>
-                <form className="mt-4">
+                <form className="mt-4" onSubmit={handleSubmit}>
                         <div className="grid gap-4">
 
                             <div className="grid gap-2">
                                 <Label htmlFor="matricule">Matricule</Label>
-                                <Input id="matricule" type="text" placeholder="Matricule" />
+                                <Input 
+                                    id="matricule" 
+                                    type="text" 
+                                    placeholder="Matricule" 
+                                    value={matricule}
+                                    onChange={(e) => setMatricule(e.target.value)}/>
                             </div>
 
                             <div className="flex gap-3">
-                                <div className="grid gap-2">
+                                <div className="grid gap-2 w-full">
                                     <Label htmlFor="name">Nom</Label>
-                                    <Input id="name" type="text" placeholder="Nom" />
+                                    <Input 
+                                        id="name" 
+                                        type="text" 
+                                        placeholder="Nom" 
+                                        value={lastname}
+                                        onChange={(e) => setLastname(e.target.value)}/>
                                 </div>
                                 <div className="grid gap-2">
                                     <Label htmlFor="prename">Prenom</Label>
-                                    <Input id="prename" type="text" placeholder="Prenom" />
+                                    <Input 
+                                        id="prename" 
+                                        type="text" 
+                                        placeholder="Prenom"
+                                        value={firstname}
+                                        onChange={(e) => setFirstname(e.target.value)} 
+                                    />
                                 </div>
                             </div>
 
                             <div className="grid gap-2">
                                 <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" placeholder="[EMAIL_ADDRESS]" />
+                                <Input 
+                                    id="email"  
+                                    type="email" 
+                                    placeholder="[EMAIL_ADDRESS]" 
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                />
                             </div>
                             <div className="grid gap-2">
                                 <Label htmlFor="password">Password</Label>
-                                <Input id="password" type="password" />
+                                <Input 
+                                    id="password" 
+                                    type="password" 
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)} 
+                                />
+                            </div>
+
+                            <div className="grid gap-2">
+                                {/*phone number*/}
+                                <Label htmlFor="phone">Téléphone</Label>
+                                <Input 
+                                    id="phone" 
+                                    type="text" 
+                                    placeholder="Téléphone" 
+                                    value={phone}
+                                    onChange={(e) => setPhone(e.target.value)} 
+                                />
+                            </div>
+                                
                             </div>
 
                             <div className="flex gap-3 ">
                                 <div className="grid gap-2 w-full">
                                     <Label htmlFor="level">Niveau</Label>
-                                    <Select>
+                                    <Select 
+                                        value={grade}
+                                        onValueChange={(value) => setGrade(value)}>
                                         <SelectTrigger>
                                             <SelectValue placeholder="Niveau" />
                                         </SelectTrigger>
@@ -61,7 +134,10 @@ const Register = () => {
                                 </div>
                                 <div className="grid gap-2 w-full">
                                     <Label htmlFor="filiere">Filiere</Label>
-                                    <Select>
+                                    <Select 
+                                        value={filiere}
+                                        onValueChange={(value) => setFiliere(value)}    
+                                        >
                                         <SelectTrigger>
                                             <SelectValue placeholder="Filiere" />
                                         </SelectTrigger>
