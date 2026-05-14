93d2c0ad98f6add82e8733a8f0744aa1a690da38
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-05-14 21:10:35 +0000

    feat: implement add team mate by team owner form with form data, ready to send it to our api

--- frontend/src/components/app/teams/AddMemberModal.tsx
+++ frontend/src/components/app/teams/AddMemberModal.tsx
@@ -9,10 +9,33 @@ import {
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Plus } from "lucide-react"
+import { Loader, Plus } from "lucide-react"
+import { useFiliere } from "@/hooks/filiere/useFiliere"
+import { useGrade } from "@/hooks/grade/useGrade"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+
 
 const AddMemberModal = () => {
+
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    grade: "",
+    filiere: "",
+  })
   const [open, setOpen] = useState(false)
+  const {data: filieres, isLoading:isLoadingFiliere, isError: isErrorFiliere} = useFiliere()
+  const {data: grades, isLoading: isLoadingGrade, isError: isErrorGrade} = useGrade()
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+    
+  }
+
+
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -28,13 +51,65 @@ const AddMemberModal = () => {
           <DialogTitle>Nouveau membre</DialogTitle>
         </DialogHeader>
 
-        <form className="grid gap-4 mt-4">
-          <Input placeholder="Prénom" />
-          <Input placeholder="Nom" />
-          <Input placeholder="Email" />
-          <Input placeholder="Téléphone" />
-          <Input placeholder="Grade" />
-          <Input placeholder="Filière" />
+        <form 
+          onSubmit={handleSubmit}
+          className="grid gap-4 mt-4"
+        >
+
+          <Input 
+            placeholder="Prénom"
+            value={form.firstName}
+            onChange={(e) => setForm({...form, firstName: e.target.value})} />
+          <Input 
+            placeholder="Nom"
+            value={form.lastName}
+            onChange={(e) => setForm({...form, lastName: e.target.value})} />
+          <Input 
+            placeholder="Email"
+            value={form.email}
+            onChange={(e) => setForm({...form, email: e.target.value})} />
+          <Input 
+            placeholder="Téléphone"
+            value={form.phone}
+            onChange={(e) => setForm({...form, phone: e.target.value})} />
+
+          {isErrorFiliere || isErrorGrade ? (
+            <p className="text-destructive">Erreur de chargement</p>
+          ) : (
+            <></>
+          )}
+
+          {isLoadingFiliere || isLoadingGrade ? (
+            <Loader/>
+          ) : (
+            <>
+            <Select >
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Grade" />
+              </SelectTrigger>
+              <SelectContent>
+                {grades?.map((grade) => (
+                  <SelectItem key={grade.value} value={grade.value}>
+                    {grade.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+              </Select>
+
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Filière" />
+              </SelectTrigger>
+              <SelectContent>
+                {filieres?.map((filiere) => (
+                  <SelectItem key={filiere.value} value={filiere.value}>
+                    {filiere.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            </>
+          )}
 
           <Button className="w-full mt-2">
             Ajouter
---
+++ frontend/src/types/teamMate.ts
@@ -0,0 +1,30 @@
+
+
+export interface TeamMateRequest {
+    firstName: string
+    lastName: string
+    email: string
+    phone: string
+    grade: string
+    filiere: string
+}
+
+export interface AddTeamMateResponse {
+    id: number
+    firstName: string
+    lastName: string
+    email: string
+    phone: string
+    grade: string
+    filiere: string
+}
+
+export interface UpdateTeamMateForm {
+    firstName: string
+    lastName: string
+    email: string
+    phone: string
+    grade: string
+    filiere: string
+}
+
